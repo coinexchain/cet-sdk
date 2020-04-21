260b417f945ddab5ffeff1c629ead8516c1f7856
ludete <yyxyong@163.com>
2020-04-21 11:12:35 +0800

    Add trade-server consumer

--- trade_server/consumer.go
+++ trade_server/consumer.go
@@ -1,1 +1,35 @@
 package trade_server
+
+import (
+	"time"
+
+	"github.com/coinexchain/trade-server/server"
+)
+
+type Consumer struct {
+	*server.TradeConsumerWithDirTail
+	doneHeightCh chan<- int64
+	dirName      string
+}
+
+func NewConsumer(tradeConsumerWithDirTail *server.TradeConsumerWithDirTail, doneHeightCh chan<- int64) *Consumer {
+	return &Consumer{TradeConsumerWithDirTail: tradeConsumerWithDirTail, doneHeightCh: doneHeightCh}
+}
+
+func (c *Consumer) work() {
+	c.Consume()
+	go c.tickDoneHeight()
+}
+
+func (c *Consumer) tickDoneHeight() {
+	tick := time.NewTicker(60 * time.Second)
+	defer tick.Stop()
+
+	for {
+		<-tick.C
+		doneHeight := c.GetLeastHeight()
+		if doneHeight != 0 {
+			c.doneHeightCh <- doneHeight
+		}
+	}
+}
