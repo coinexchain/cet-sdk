3c407fa7b5536b8dbf8f0e4af6880f620af51d5e
selectAname <jy09091001@163.com>
2020-10-30 13:04:03 +0800

    add fee params for autoswap

--- modules/autoswap/internal/keepers/keeper.go
+++ modules/autoswap/internal/keepers/keeper.go
@@ -3,6 +3,7 @@ package keepers
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/params"
+	"math/big"
 
 	"github.com/coinexchain/cet-sdk/modules/autoswap/internal/types"
 )
@@ -10,6 +11,7 @@ import (
 type Keeper struct {
 	storeKey      sdk.StoreKey
 	paramSubspace params.Subspace
+	sk            types.SupplyKeeper
 	FactoryInterface
 	IPoolKeeper
 	IPairKeeper
@@ -22,4 +24,43 @@ func (keeper *Keeper) SetParams(ctx sdk.Context, params types.Params) {
 func (keeper *Keeper) GetParams(ctx sdk.Context) (param types.Params) {
 	keeper.paramSubspace.GetParamSet(ctx, &param)
 	return
+}
+func (keeper *Keeper) GetTakerFee(ctx sdk.Context) sdk.Dec {
+	return sdk.NewDec(keeper.GetParams(ctx).TakerFeeRateRate).QuoInt64(types.DefaultFeePrecision)
+}
+
+func (keeper *Keeper) GetMakerFee(ctx sdk.Context) sdk.Dec {
+	return sdk.NewDec(keeper.GetParams(ctx).MakerFeeRateRate).QuoInt64(types.DefaultFeePrecision)
+}
+func (keeper *Keeper) GetFeeToValidator(ctx sdk.Context) sdk.Dec {
+	param := keeper.GetParams(ctx)
+	return sdk.NewDec(param.FeeToValidator).QuoInt64(param.FeeToValidator + param.FeeToPool)
+}
+func (keeper Keeper) SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) sdk.Error {
+	return keeper.sk.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, amt)
+}
+func (keeper *Keeper) AllocateFeeToValidator(ctx sdk.Context, lastK *sdk.Int, info PoolInfo) {
+	if !lastK.IsZero() {
+		k := info.moneyAmmReserve.Mul(info.stockAmmReserve).BigInt()
+		var rootK, rootLastK *big.Int
+		rootK.Sqrt(k)
+		rootLastK.Sqrt(lastK.BigInt())
+		subK := sdk.NewIntFromBigInt(rootK.Sub(rootK, rootLastK))
+
+		if rootK.Cmp(rootLastK) == 1 {
+			param := keeper.GetParams(ctx)
+			numerator := subK.MulRaw(param.FeeToValidator)
+			denominator := sdk.NewIntFromBigInt(rootK).MulRaw(param.FeeToPool).
+				Add(sdk.NewIntFromBigInt(rootLastK).MulRaw(param.FeeToValidator))
+			moneyToValidator := info.moneyAmmReserve.Mul(numerator).Quo(denominator)
+			stockToValidator := info.stockAmmReserve.Mul(numerator).Quo(denominator)
+			// todo: stock,money := parseSymbol(info.symbol)
+			if moneyToValidator.IsPositive() {
+				// todo: transfer money to auth.feecollector
+			}
+			if stockToValidator.IsPositive() {
+				// todo: transfer stock to auth.feecollector
+			}
+		}
+	}
 }
--- modules/autoswap/internal/types/params.go
+++ modules/autoswap/internal/types/params.go
@@ -7,7 +7,26 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/params"
 )
 
+const (
+	DefaultFeePrecision   = 10000
+	DefaultTakerFeeRate   = 30
+	DefaultMakerFeeRate   = 50
+	DefaultFeeToPool      = 6
+	DefaultFeeToValidator = 4
+)
+
+var (
+	keyTakerFeeRateRate = []byte("TakerFeeRate")
+	keyMakerFeeRate     = []byte("MakerFeeRate")
+	keyFeeToPool        = []byte("FeeToPool")
+	keyFeeToValidator   = []byte("FeeToValidator")
+)
+
 type Params struct {
+	TakerFeeRateRate int64
+	MakerFeeRateRate int64
+	FeeToPool        int64
+	FeeToValidator   int64
 }
 
 func ParamKeyTable() params.KeyTable {
@@ -15,14 +34,34 @@ func ParamKeyTable() params.KeyTable {
 }
 
 func DefaultParams() Params {
-	return Params{}
+	return Params{
+		TakerFeeRateRate: DefaultTakerFeeRate,
+		MakerFeeRateRate: DefaultMakerFeeRate,
+		FeeToPool:        DefaultFeeToPool,
+		FeeToValidator:   DefaultFeeToValidator,
+	}
 }
 
 func (p *Params) ParamSetPairs() params.ParamSetPairs {
-	return params.ParamSetPairs{}
+	return params.ParamSetPairs{
+		{Key: keyTakerFeeRateRate, Value: p.TakerFeeRateRate},
+		{Key: keyMakerFeeRate, Value: p.MakerFeeRateRate},
+		{Key: keyFeeToPool, Value: p.FeeToPool},
+		{Key: keyFeeToValidator, Value: p.FeeToValidator},
+	}
 }
 
-func (p *Params) ValidateGenesis() error { return nil }
+func (p *Params) ValidateGenesis() error {
+	if p.TakerFeeRateRate <= 0 || p.MakerFeeRateRate <= 0 || p.FeeToPool <= 0 || p.FeeToValidator <= 0 {
+		return fmt.Errorf("parameter can not be a negative number: TakerFeeRate: %d, "+
+			"MakerFeeRate: %d, FeeToPool: %d, FeeToValidator: %d", p.TakerFeeRateRate, p.MakerFeeRateRate, p.FeeToPool, p.FeeToValidator)
+	}
+	if p.TakerFeeRateRate >= DefaultFeePrecision || p.MakerFeeRateRate >= DefaultFeePrecision {
+		return fmt.Errorf("FeeRate should be less than 1. TakerFeeRate: %d, MakerFeeRate: %d",
+			p.TakerFeeRateRate, p.MakerFeeRateRate)
+	}
+	return nil
+}
 
 func (p Params) Equal(p2 Params) bool {
 	bz1 := ModuleCdc.MustMarshalBinaryLengthPrefixed(&p)
@@ -31,5 +70,13 @@ func (p Params) Equal(p2 Params) bool {
 }
 
 func (p Params) String() string {
-	return fmt.Sprintf(`autoswap Params:`)
+	return fmt.Sprintf(`autoswap Params:
+	TakerFeeRate: %d,
+	MakerFeeRate: %d,
+	FeeToPool: %d,
+	FeeToValidator: %d`,
+		p.TakerFeeRateRate,
+		p.MakerFeeRateRate,
+		p.FeeToPool,
+		p.FeeToValidator)
 }
