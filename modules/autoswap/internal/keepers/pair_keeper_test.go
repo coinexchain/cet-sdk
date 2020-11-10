7e84b7b4cecb9f3ba3ddf51f0120bcc8a335fb8b
selectAname <jy09091001@163.com>
2020-11-10 17:56:52 +0800

    add test for AllocateFeeToValidatorAndPool

--- modules/autoswap/internal/keepers/pair_keeper_test.go
+++ modules/autoswap/internal/keepers/pair_keeper_test.go
@@ -1,6 +1,9 @@
 package keepers_test
 
 import (
+	"github.com/coinexchain/cet-sdk/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,6 +14,10 @@ import (
 
 	"github.com/coinexchain/cet-sdk/modules/autoswap/internal/keepers"
 	testapp "github.com/coinexchain/cet-sdk/testapp"
+)
+
+var (
+	sender = testutil.ToAccAddress("sender")
 )
 
 func TestPairKeeper_SetOrder(t *testing.T) {
@@ -25,10 +32,10 @@ func TestPairKeeper_SetOrder(t *testing.T) {
 		PrevKey:         [3]int64{1, 2, 3},
 		MinOutputAmount: sdk.NewInt(19),
 		OrderBasic: types.OrderBasic{
-			MarketSymbol:    "stock/money",
-			IsBuy:           false,
-			IsLimitOrder:    false,
-			Amount:          sdk.NewInt(1999999),
+			MarketSymbol: "stock/money",
+			IsBuy:        false,
+			IsLimitOrder: false,
+			Amount:       sdk.NewInt(1999999),
 		},
 	}
 	k.SetOrder(ctx, &order)
@@ -38,4 +45,29 @@ func TestPairKeeper_SetOrder(t *testing.T) {
 	require.EqualValues(t, recordOrder.OrderID, order.OrderID)
 	require.EqualValues(t, recordOrder.Price, order.Price)
 	require.EqualValues(t, recordOrder.NextOrderID, order.NextOrderID)
+}
+
+func TestPairKeeper_AllocateFeeToValidatorAndPool(t *testing.T) {
+	app := testapp.NewTestApp()
+	ctx := app.NewCtx()
+	app.AutoSwapKeeper.SetParams(ctx, types.DefaultParams())
+	keeper := app.AutoSwapKeeper.IPairKeeper.(*keepers.PairKeeper)
+
+	//setup accounts
+	app.AccountKeeper.SetAccount(ctx, supply.NewEmptyModuleAccount(types.PoolModuleAcc))
+	app.AccountKeeper.SetAccount(ctx, supply.NewEmptyModuleAccount(auth.FeeCollectorName))
+	baseAcc := auth.NewBaseAccount(sender, sdk.NewCoins(sdk.NewCoin("money", sdk.NewInt(100)),
+		sdk.NewCoin("stock", sdk.NewInt(100))), nil, 0, 0)
+	app.AccountKeeper.SetAccount(ctx, baseAcc)
+
+	err := keeper.AllocateFeeToValidatorAndPool(ctx, "money", sdk.NewInt(5), sender)
+	require.Nil(t, err)
+
+	err = keeper.AllocateFeeToValidatorAndPool(ctx, "stock", sdk.NewInt(10), sender)
+	require.Nil(t, err)
+
+	poolCoins := app.AccountKeeper.GetAccount(ctx, supply.NewModuleAddress(types.PoolModuleAcc)).GetCoins()
+	feeCollectorCoins := app.AccountKeeper.GetAccount(ctx, supply.NewModuleAddress(auth.FeeCollectorName)).GetCoins()
+	require.Equal(t, sdk.NewCoins(sdk.NewCoin("money", sdk.NewInt(3)), sdk.NewCoin("stock", sdk.NewInt(6))), poolCoins)
+	require.Equal(t, sdk.NewCoins(sdk.NewCoin("money", sdk.NewInt(2)), sdk.NewCoin("stock", sdk.NewInt(4))), feeCollectorCoins)
 }
